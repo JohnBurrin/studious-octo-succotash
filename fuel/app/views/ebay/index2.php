c7de7aac3bfbf44998170d1de5b188d36a5d2feb
John <john@synergitech.co.uk>
2020-09-11 12:12:07 +0100

    Lint

--- fuel/app/views/ebay/index2.php
+++ fuel/app/views/ebay/index2.php
@@ -1,16 +1,16 @@
 <?php echo Pagination::instance('mypagination')->render(); ?>
  <div class="card-deck justify-content-center">
-  <?php foreach ($this->data['item_data'] as $row_data) {
-      $listingInfo = json_decode($row_data['listingInfo']);
-      $paymentMethod = json_decode($row_data['paymentMethod']);
-      $shippingInfo = json_decode($row_data['shippingInfo'],true);
-      $condition = json_decode($row_data['condition']);
-      $sellingStatus = json_decode($row_data['sellingStatus'],true);
-      $date = $listingInfo->endTime[0];
-      $endDateTime = new DateTime($date);
-      $now = new DateTime();
-      $interval = $now->diff($endDateTime);
-     ?>
+    <?php foreach ($this->data['item_data'] as $row_data) {
+        $listingInfo = json_decode($row_data['listingInfo']);
+        $paymentMethod = json_decode($row_data['paymentMethod']);
+        $shippingInfo = json_decode($row_data['shippingInfo'], true);
+        $condition = json_decode($row_data['condition']);
+        $sellingStatus = json_decode($row_data['sellingStatus'], true);
+        $date = $listingInfo->endTime[0];
+        $endDateTime = new DateTime($date);
+        $now = new DateTime();
+        $interval = $now->diff($endDateTime);
+        ?>
      <div class="col-auto mb-3">
         <div class="card" style="width: 20rem;">
             <a href="<?php echo $row_data['viewItemURL'];?>">
@@ -21,12 +21,12 @@
             <p class="card-text"><?php echo $row_data['title']; ?></p>
             <ul class="list-group list-group-flush">
                  <li class="list-group-item">Price: <?= $sellingStatus['currentPrice'][0]['__value__'] ?> <?= $sellingStatus['currentPrice'][0]['@currencyId']?> <br />
-                     Shipping: <?= $shippingInfo['shippingServiceCost'][0]['__value__'] ?> <?= $shippingInfo['shippingServiceCost'][0]['@currencyId']?></li>
-                 <?php
-                 (isset($listingInfo->watchCount) && $listingInfo->watchCount[0] > 1) ? $plural ="s" : $plural = "";
-                 (isset($row_data['postalCode'])) ? $postalCode = ", " . $row_data['postalCode'] : $postalCode = "";
-                 $postalCode = substr_replace($postalCode, " ", -3, 0);
-                  ?>
+                    Shipping: <?= $shippingInfo['shippingServiceCost'][0]['__value__'] ?> <?= $shippingInfo['shippingServiceCost'][0]['@currencyId']?></li>
+                <?php
+                    (isset($listingInfo->watchCount) && $listingInfo->watchCount[0] > 1) ? $plural ="s" : $plural = "";
+                    (isset($row_data['postalCode'])) ? $postalCode = ", " . $row_data['postalCode'] : $postalCode = "";
+                    $postalCode = substr_replace($postalCode, " ", -3, 0);
+                ?>
             <li class="list-group-item"><p class="card-text"><?php echo $row_data['location']; ?> <?= $postalCode ?></p></li>
               <li class="list-group-item"><?= (isset($listingInfo->watchCount)) ? $listingInfo->watchCount[0] . " watcher".$plural : "0 watchers" ?></li>
               <!--li class="list-group-item">Best Offer is<?= ($listingInfo->bestOfferEnabled[0] === "true" ? "" : "n't" )?> available</li>
@@ -34,7 +34,7 @@
               <li class="list-group-item">Condition:<?= (isset($condition->conditionDisplayName[0]) ? $condition->conditionDisplayName[0] : "") ?></li>
               <li class="list-group-item">
               <?php if (isset($row_data['storeName'])) {
-                  echo "<a href=\"" .  $row_data['storeURL']. "\">" . $row_data['storeName'] ."</a>";
+                    echo "<a href=\"" .  $row_data['storeURL']. "\">" . $row_data['storeName'] ."</a>";
               } else {
                   echo "Seller: " .$row_data['sellerUserName'];
               }?>
@@ -48,7 +48,7 @@
           </div>
         </div>
     </div>
-  <?php } ?>
+    <?php } ?>
 </div>
 
 <?php echo Pagination::instance('mypagination')->render(); ?>
