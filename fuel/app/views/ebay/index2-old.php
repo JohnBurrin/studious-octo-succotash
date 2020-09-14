7ffe182594bb701734e2f8129e596ff92193db82
John <john@synergitech.co.uk>
2020-09-14 12:24:54 +0100

    New mobile UI

---
+++ fuel/app/views/ebay/index2-old.php
@@ -0,0 +1,56 @@
+<?php echo Pagination::instance('mypagination')->render(); ?>
+ <div class="card-deck justify-content-center">
+    <?php foreach ($this->data['item_data'] as $row_data) {
+        $listingInfo = json_decode($row_data['listingInfo']);
+        $paymentMethod = json_decode($row_data['paymentMethod']);
+        $shippingInfo = json_decode($row_data['shippingInfo'], true);
+        $condition = json_decode($row_data['condition']);
+        $sellingStatus = json_decode($row_data['sellingStatus'], true);
+        $date = $listingInfo->endTime[0];
+        $endDateTime = new DateTime($date);
+        $now = new DateTime();
+        $interval = $now->diff($endDateTime);
+        ?>
+     <div class="col-auto mb-3">
+        <div class="card" style="width: 20rem;">
+            <a href="<?php echo $row_data['viewItemURL'];?>">
+                <img class="card-img-top" src="<?php echo $row_data['galleryURL'];?>" alt="...">
+            </a>
+          <div class="card-body">
+            <h5 class="card-title"></h5>
+            <p class="card-text"><?php echo $row_data['title']; ?></p>
+            <ul class="list-group list-group-flush">
+                 <li class="list-group-item">Price: <?= $sellingStatus['currentPrice'][0]['__value__'] ?> <?= $sellingStatus['currentPrice'][0]['@currencyId']?> <br />
+                    Shipping: <?= $shippingInfo['shippingServiceCost'][0]['__value__'] ?> <?= $shippingInfo['shippingServiceCost'][0]['@currencyId']?></li>
+                <?php
+                    (isset($listingInfo->watchCount) && $listingInfo->watchCount[0] > 1) ? $watcherplural ="s" : $watcherplural = "";
+                    (isset($sellingStatus['bidCount']) && $sellingStatus['bidCount'][0] > 1) ? $bidderplural ="s" : $bidderplural = "";
+                    (isset($row_data['postalCode'])) ? $postalCode = ", " . $row_data['postalCode'] : $postalCode = "";
+                    $postalCode = substr_replace($postalCode, " ", -3, 0);
+                ?>
+            <li class="list-group-item"><p class="card-text"><?php echo $row_data['location']; ?> <?= $postalCode ?></p></li>
+              <li class="list-group-item"><?= (isset($listingInfo->watchCount)) ? $listingInfo->watchCount[0] . " watcher".$watcherplural : "0 watchers" ?></li>
+              <li class="list-group-item"><?= (isset($sellingStatus['bidCount'])) ? $sellingStatus['bidCount'][0] . " bid".$bidderplural : "0 bids" ?></li>
+              <!--li class="list-group-item">Best Offer is<?= ($listingInfo->bestOfferEnabled[0] === "true" ? "" : "n't" )?> available</li>
+              <li class="list-group-item">Buy it now is<?= ($listingInfo->buyItNowAvailable[0] === "true" ? "" : "n't" )?> available</li-->
+              <li class="list-group-item">Condition:<?= (isset($condition->conditionDisplayName[0]) ? $condition->conditionDisplayName[0] : "") ?></li>
+              <li class="list-group-item">
+              <?php if (isset($row_data['storeName'])) {
+                    echo "<a href=\"" .  $row_data['storeURL']. "\">" . $row_data['storeName'] ."</a>";
+              } else {
+                  echo "Seller: " .$row_data['sellerUserName'];
+              }?>
+              (<?=$row_data['feedbackScore']?>)
+              </li>
+              <li class="list-group-item"><?= ($interval->format("%R") =="-") ? "<b class=\"text-danger\">Ended</b>" : "Time left " . $interval->format('%a days %Hh %Im %Ss')?></li>
+            </ul>
+            <div class="col text-center mt-2">
+              <a href="<?php echo $row_data['viewItemURL'];?>" class="btn btn-primary">View On eBay</a>
+            </div>
+          </div>
+        </div>
+    </div>
+    <?php } ?>
+</div>
+
+<?php echo Pagination::instance('mypagination')->render(); ?>
--- fuel/app/views/template.php
+++ fuel/app/views/template.php
@@ -1,35 +1,135 @@
-<!DOCTYPE html>
-<html>
+
+        </div>
+    </div>
+</section>
+<!-- Team -->	<script type="text/javascript">
+	//No Js required	</script>
+</body>
+</html><!DOCTYPE html>
+<html lang="en">
 <head>
     <meta charset="utf-8">
-    <title><?php echo $title; ?></title>
-    <?php echo Asset::css('bootstrap.css'); ?>
-    <style>
-        body { margin: 40px; }
-    </style>
-    <script>window._epn = {campaign: 5338733485};</script>
-<script src="https://epnt.ebay.com/static/epn-smart-tools.js"></script>
-<!-- Global site tag (gtag.js) - Google Analytics -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=UA-177804611-1"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
+    <meta name="robots" content="noindex, nofollow">
 
-  gtag('config', 'UA-177804611-1');
-</script>
+    <title>Team design card flipper using bootstrap 4 - Bootsnipp.com</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+    <style type="text/css">
+    /* FontAwesome for working BootSnippet :> */
+
+@import url('https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css');
+#team {
+    background: #eee !important;
+}
+
+.btn-primary:hover,
+.btn-primary:focus {
+    background-color: #108d6f;
+    border-color: #108d6f;
+    box-shadow: none;
+    outline: none;
+}
+
+.btn-primary {
+    color: #fff;
+    background-color: #007b5e;
+    border-color: #007b5e;
+}
+
+section {
+    padding: 60px 0;
+}
+
+section .section-title {
+    text-align: center;
+    color: #007b5e;
+    margin-bottom: 50px;
+    text-transform: uppercase;
+}
+
+#team .card {
+    border: none;
+    background: #ffffff;
+}
+
+.frontside {
+    position: relative;
+    -webkit-transform: rotateY(0deg);
+    -ms-transform: rotateY(0deg);
+    z-index: 2;
+    margin-bottom: 30px;
+}
+
+.backside {
+    position: absolute;
+    top: 0;
+    left: 0;
+    background: white;
+    -webkit-transform: rotateY(-180deg);
+    -moz-transform: rotateY(-180deg);
+    -o-transform: rotateY(-180deg);
+    -ms-transform: rotateY(-180deg);
+    transform: rotateY(-180deg);
+    -webkit-box-shadow: 5px 7px 9px -4px rgb(158, 158, 158);
+    -moz-box-shadow: 5px 7px 9px -4px rgb(158, 158, 158);
+    box-shadow: 5px 7px 9px -4px rgb(158, 158, 158);
+}
+
+.frontside,
+.backside {
+    -webkit-backface-visibility: hidden;
+    -moz-backface-visibility: hidden;
+    -ms-backface-visibility: hidden;
+    backface-visibility: hidden;
+    -webkit-transition: 1s;
+    -webkit-transform-style: preserve-3d;
+    -moz-transition: 1s;
+    -moz-transform-style: preserve-3d;
+    -o-transition: 1s;
+    -o-transform-style: preserve-3d;
+    -ms-transition: 1s;
+    -ms-transform-style: preserve-3d;
+    transition: 1s;
+    transform-style: preserve-3d;
+}
+
+.frontside .card,
+.backside .card {
+    min-height: 312px;
+}
+
+.backside .card a {
+    font-size: 18px;
+    color: #007b5e !important;
+}
+
+.frontside .card .card-title,
+.backside .card .card-title {
+    color: #007b5e !important;
+}
+
+.frontside .card .card-body img {
+
+}    </style>
+    <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
 
 </head>
 <body>
-    <div class="container-fluid mt-4">
+    <!-- Team -->
+<section id="team" class="pb-5">
+    <div class="container">
+        <h5 class="section-title h1"><?php echo $title; ?></h5>
+        <p class="text-right"><a href="<?=$link['url']?>"><?=$link['title']?></a></p>
+        <?php echo Pagination::instance('mypagination')->render(); ?>
+        <div class="row">
+  <?php echo $content; ?>
 
-            <h1><?php echo $title; ?></h1>
-            <p class="text-right"><a href="<?=$link['url']?>"><?=$link['title']?></a></p>
-            <hr>
-            <?php echo $content; ?>
-        <footer>
-            <p class="pull-right">Page rendered in {exec_time}s using {mem_usage}mb of memory.</p>
-        </footer>
-    </div>
-</body>
-</html>
+          </div>
+          <?php echo Pagination::instance('mypagination')->render(); ?>
+      </div>
+  </section>
+  <!-- Team -->	<script type="text/javascript">
+  	//No Js required	</script>
+  </body>
+  </html>
