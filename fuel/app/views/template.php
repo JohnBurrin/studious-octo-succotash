a8fc06b9c8ba3e46ea303d195e8d731a1e27a71b
John <john@synergitech.co.uk>
2020-09-15 08:05:04 +0100

    Remove all the robots

--- fuel/app/views/template.php
+++ fuel/app/views/template.php
@@ -2,7 +2,6 @@
 <html lang="en">
 <head>
     <meta charset="utf-8">
-    <meta name="robots" content="noindex, nofollow">
 
     <title><?php echo $title; ?></title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
