298747c1f4bf647b9346d9ef9ecff537794d2e91
John <john@synergitech.co.uk>
2020-09-18 07:33:31 +0100

    moce css to file

--- fuel/app/views/template.php
+++ fuel/app/views/template.php
@@ -5,135 +5,29 @@
 
     <title><?php echo $title; ?></title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
+
     <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
-    <style type="text/css">
-    /* FontAwesome for working BootSnippet :> */
+    <?php
+        echo Asset::css('app.css', array(), null, true);
+    ?>
 
-    @import url('https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css');
-    #team {
-        background: #eee !important;
-    }
-
-    .btn-primary:hover,
-    .btn-primary:focus {
-        background-color: #108d6f;
-        border-color: #108d6f;
-        box-shadow: none;
-        outline: none;
-    }
-
-    .btn-primary {
-        color: #fff;
-        background-color: #007b5e;
-        border-color: #007b5e;
-    }
-
-    section {
-        padding: 60px 0;
-    }
-
-    section .section-title {
-        text-align: center;
-        color: #007b5e;
-        margin-bottom: 50px;
-        text-transform: uppercase;
-    }
-
-    #team .card {
-        border: none;
-        background: #ffffff;
-    }
-
-    .frontside {
-        position: relative;
-        -webkit-transform: rotateY(0deg);
-        -ms-transform: rotateY(0deg);
-        z-index: 2;
-        margin-bottom: 30px;
-    }
-
-    .backside {
-        position: absolute;
-        top: 0;
-        left: 0;
-        background: white;
-        -webkit-transform: rotateY(-180deg);
-        -moz-transform: rotateY(-180deg);
-        -o-transform: rotateY(-180deg);
-        -ms-transform: rotateY(-180deg);
-        transform: rotateY(-180deg);
-        -webkit-box-shadow: 5px 7px 9px -4px rgb(158, 158, 158);
-        -moz-box-shadow: 5px 7px 9px -4px rgb(158, 158, 158);
-        box-shadow: 5px 7px 9px -4px rgb(158, 158, 158);
-    }
-
-    .frontside,
-    .backside {
-        -webkit-backface-visibility: hidden;
-        -moz-backface-visibility: hidden;
-        -ms-backface-visibility: hidden;
-        backface-visibility: hidden;
-        -webkit-transition: 1s;
-        -webkit-transform-style: preserve-3d;
-        -moz-transition: 1s;
-        -moz-transform-style: preserve-3d;
-        -o-transition: 1s;
-        -o-transform-style: preserve-3d;
-        -ms-transition: 1s;
-        -ms-transform-style: preserve-3d;
-        transition: 1s;
-        transform-style: preserve-3d;
-    }
-
-    .frontside .card,
-    .backside .card {
-        min-height: 312px;
-    }
-
-    .backside .card a {
-        font-size: 18px;
-        color: #007b5e !important;
-    }
-
-    .frontside .card .card-title,
-    .backside .card .card-title {
-        color: #007b5e !important;
-    }
-
-    .frontside .card .card-body img {
-
-    }
-    .page-link {
-        color: #0000FF !important;
-    }
-
-    .page-item.active .page-link {
-        z-index: 1;
-        color: #fff !important;
-        background-color: #007b5e;
-        border-color: #007b5e;
-    }
-    a {
-        color: #0000FF;
-    };
-</style>
-<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-<script src="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
-
-<script>window._epn = {campaign: 5338733485};</script>
-
-<script src="https://epnt.ebay.com/static/epn-smart-tools.js"></script>
-<!-- Global site tag (gtag.js) - Google Analytics -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=UA-177804611-1"></script>
-
-<script>
-    window.dataLayer = window.dataLayer || [];
-    function gtag(){dataLayer.push(arguments);}
-    gtag('js', new Date());
-
-    gtag('config', 'UA-177804611-1');
-</script>
     <script data-ad-client="ca-pub-8371692745987188" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+    <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+
+    <script>window._epn = {campaign: 5338733485};</script>
+
+    <script src="https://epnt.ebay.com/static/epn-smart-tools.js"></script>
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-177804611-1"></script>
+
+    <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'UA-177804611-1');
+    </script>
 </head>
 <body>
     <!-- Team -->
