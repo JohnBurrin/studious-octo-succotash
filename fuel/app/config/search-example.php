8f7be762b6a04c2ae48e0ab5759fff7e21953c0b
John <john@synergitech.co.uk>
2020-09-29 08:19:41 +0100

    Update date search example

--- fuel/app/config/search-example.php
+++ fuel/app/config/search-example.php
@@ -4,6 +4,22 @@
             array('keywords' => 'Honda Superdream CB250N CB400N', "categoryId" => null),
             array('keywords' => 'superdream', "categoryId" => 422)
         ),
-        'table' => 'k100'
+        'table' => 'superdream',
+        'template' => array (
+
+            'home' =>array (
+                'link' => ['title' => "Bikes", 'url' => "bikes"],
+                'title' => 'Honda CB250N / CB400N Superdream spare parts for sale on eBay',
+                'description' => 'This page contains a list of Honda CB250n and CB400n spare parts availble for sale on eBay',
+                'keywords' => 'Honda, CB250N, CB400N, Superdream, spare parts, eBay',
+            ),
+            'bikes' =>array (
+                'link' => ['title' => "Parts", 'url' => "/"],
+                'title' => 'Honda CB250N / CB400N Superdream motorcyles for sale on eBay',
+                'description' => 'This page contains a list of Honda CB250n and CB400n motorcycles availble for sale on eBay',
+                'keywords' => 'Honda, CB250N, CB400N, Superdream, motorcycles, bikes, eBay',
+            )
+
+        )
     );
 ?>
