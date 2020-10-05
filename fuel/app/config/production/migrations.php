b6b76c6b7c211d440a547a29cd07bbee2ba842d3
John B <jonnyb3010@gmail.com>
2020-10-05 07:46:32 +0000

    Live update

---
+++ fuel/app/config/production/migrations.php
@@ -0,0 +1,24 @@
+<?php
+return array(
+  'version' => 
+  array(
+    'app' => 
+    array(
+      'default' => 
+      array(
+        0 => '001_create_table_superdream',
+        1 => '002_create_table_k100',
+        2 => '003_create_table_gs500',
+      ),
+    ),
+    'module' => 
+    array(
+    ),
+    'package' => 
+    array(
+    ),
+  ),
+  'folder' => 'migrations/',
+  'table' => 'migration',
+  'flush_cache' => false,
+);
