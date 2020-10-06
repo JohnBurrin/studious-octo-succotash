4383fe294a86c5c2fb68f0e1bdb12bdabdfc165c
John B <jonnyb3010@gmail.com>
2020-10-06 07:42:34 +0000

    Add CB750

--- fuel/app/config/production/migrations.php
+++ fuel/app/config/production/migrations.php
@@ -9,6 +9,7 @@ return array(
         0 => '001_create_table_superdream',
         1 => '002_create_table_k100',
         2 => '003_create_table_gs500',
+        3 => '004_create_table_cb750',
       ),
     ),
     'module' => 
